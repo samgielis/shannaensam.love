8184f1be3003db8e611a196abc0b39d0b8363595
Sam Gielis <samgielis@gmail.com>
2023-05-30 20:28:09 +0200

    Fix fullscreen size

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,20 @@ import {
 import { customTheme } from "./theme";
 import { MoreInfoModal } from "./components/MoreInfoModal";
 
+const fullcreenProps = {h: '100vh', w: '100vw', minH: '-webkit-fill-available'}
 export const App = () => {
   
   const {isOpen, onClose, onOpen} = useDisclosure();
 
   return <ChakraProvider theme={customTheme}>
   <Box>
-    <Image pos='absolute' w='100vw' h='100vh' objectFit={'cover'} src={`bg-xs.jpg`}  zIndex='-1' filter='blur(2px)'/>
-    <Image pos='absolute' w='100vw' h='100vh' objectFit={'cover'} srcSet={`bg-md.jpg 2048w, bg-lg.jpg 4096w, bg-full.jpg 4096w`}/>
+    <Image pos='absolute' {...fullcreenProps} objectFit={'cover'} src={`bg-xs.jpg`}  zIndex='-1' filter='blur(2px)'/>
+    <Image pos='absolute' {...fullcreenProps} objectFit={'cover'} srcSet={`bg-md.jpg 2048w, bg-lg.jpg 4096w, bg-full.jpg 4096w`}/>
       <Box
         textAlign="center"
         color="white"
         fontSize="xl"
-        h="100vh"
+        {...fullcreenProps}
         //bgImage={`image-set(url("bg-xs.jpg") .2x, url("bg-sm.jpg") .5x, url("bg-md.jpg") .8x, url("bg-lg.jpg") 1x, url("bg-full.jpg") 2x)`}
         bgSize={"cover"}
         bgPos={"center"}
