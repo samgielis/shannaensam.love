97c964f88452d380f034a7833d66e9ac3db66134
Sam Gielis <samgielis@gmail.com>
2023-08-03 23:12:29 +0200

    Validity check cantmakeitmodal

--- src/components/CantMakeItModal/index.tsx
+++ src/components/CantMakeItModal/index.tsx
@@ -16,21 +16,35 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { useLanguage } from "../../context/LanguageContext";
 import { trans, translations } from "../../translations";
 
 type CantMakeItModalProps = Pick<ModalProps, "isOpen" | "onClose"> & {};
 export const CantMakeItModal = ({ ...props }: CantMakeItModalProps) => {
   const { language } = useLanguage();
-  const initialRef = React.useRef(null);
+  const formRef = React.useRef<HTMLFormElement>(null);
+  const nameRef = React.useRef(null);
+  const emailRef = React.useRef(null);
+
+  const [canContinue, setCanContinue] = useState(false);
+
+  const checkValidity = () => {
+    setCanContinue(!!formRef.current?.checkValidity());
+  };
 
   return (
     <Modal {...props} size={"md"} isCentered closeOnOverlayClick={false}>
       <ModalOverlay />
       <ModalContent maxH={"full"} overflow="auto">
         {/* When you change this, change helper form in index as well */}
-        <form name="cancellation" method="POST" data-netlify="true">
+        <form
+          ref={formRef}
+          name="cancellation"
+          method="POST"
+          data-netlify="true"
+          onChange={checkValidity}
+        >
           <ModalHeader>
             {trans(translations.cantMakeIt.title, language)}
           </ModalHeader>
@@ -40,12 +54,12 @@ export const CantMakeItModal = ({ ...props }: CantMakeItModalProps) => {
               <Text>{trans(translations.cantMakeIt.message, language)}</Text>
               <FormControl isRequired>
                 <FormLabel>{trans(translations.name, language)}</FormLabel>
-                <Input ref={initialRef} type="text" name="name" />
+                <Input ref={nameRef} type="text" name="name" />
               </FormControl>
 
               <FormControl isRequired>
                 <FormLabel>{trans(translations.email, language)}</FormLabel>
-                <Input type="email" name="email" />
+                <Input ref={emailRef} type="email" name="email" />
               </FormControl>
 
               <FormControl>
@@ -63,7 +77,11 @@ export const CantMakeItModal = ({ ...props }: CantMakeItModalProps) => {
                 <Button colorScheme="gray" onClick={props.onClose}>
                   {trans(translations.cancel, language)}
                 </Button>
-                <Button colorScheme="green" type="submit">
+                <Button
+                  colorScheme="green"
+                  type="submit"
+                  isDisabled={!canContinue}
+                >
                   {trans(translations.confirm, language)}
                 </Button>
               </ButtonGroup>
