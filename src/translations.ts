b864dabf08bdd1c5e235f578a7c09c36403d36ba
Sam Gielis <samgielis@gmail.com>
2023-08-06 11:39:42 +0200

    Copy update

--- src/translations.ts
+++ src/translations.ts
@@ -89,10 +89,10 @@ export const translations = {
       content: {
         nl: `Op 29 september 2023 geven wij elkaar het jawoord. Dat willen we samen met jou vieren!
                 
-                Gelieve je aanwezigheid te bevestigen voor 20 augustus via deze tool.`,
+                Gelieve je aanwezigheid te bevestigen voor 20 augustus.`,
         en: `We're getting married on September 29th 2023. We'd love to celebrate with you!
                 
-                Please RSVP before August 20th using this tool.`,
+                Please RSVP before August 20th.`,
       },
       accept: {
         nl: "Ik kom!",
