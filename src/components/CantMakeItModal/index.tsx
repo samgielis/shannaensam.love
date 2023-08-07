1b96412100e3da564be2dc297efefcff80ab43a1
Sam Gielis <samgielis@gmail.com>
2023-08-07 11:55:50 +0200

    Add form name input

--- src/components/CantMakeItModal/index.tsx
+++ src/components/CantMakeItModal/index.tsx
@@ -14,6 +14,7 @@ import {
   ModalProps,
   Text,
   Textarea,
+  VisuallyHidden,
   VStack,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
@@ -87,6 +88,9 @@ export const CantMakeItModal = ({ ...props }: CantMakeItModalProps) => {
                 </Button>
               </ButtonGroup>
             </VStack>
+            <VisuallyHidden>
+              <input type="hidden" name="form-name" value="cancelled" />
+            </VisuallyHidden>
           </ModalFooter>
         </form>
       </ModalContent>
